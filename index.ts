0ce0617bf7857920b9751e8c2b365c90efafe595
Timmerom <timur.m2007@gmail.com>
2026-03-30 11:50:00 +0200

    corslib1_connection

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import mongoose from "mongoose"
 import user_router from "./userRouter"
+import cors from 'cors';
 
 
 const app = express()
 const PORT = process.env.PORT || 3000;
 const URL_DB = process.env.URL_DB || "";
 app.use(express.json( ))
+app.use(cors());
 app.use("/api",user_router)
 
 async function startApp(){
