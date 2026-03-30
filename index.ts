4efc42fc2b6f01b6bb109c4bac1ca50339905006
Timmerom <timur.m2007@gmail.com>
2026-03-30 16:35:10 +0200

    hot_fix

--- index.ts
+++ index.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import mongoose from "mongoose"
 import user_router from "./userRouter"
 import cors from 'cors';
+import * as dotenv from 'dotenv'; // 1. Импорт
 
+dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 3000;
-const URL_DB = process.env.URL_DB || "";
+const URL_DB = process.env.URL_DB as string;
+
 app.use(express.json( ))
 app.use(cors());
 app.use("/api",user_router)
