1b2218308fcf4ae514481f4645ce302b79222c0b
Timmerom <timur.m2007@gmail.com>
2026-03-30 12:26:59 +0200

    telephone_fix

--- users.ts
+++ users.ts
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const User = new mongoose.Schema({
     email: {type: String, required: true},
     password: {type: String, required: true},
-    telephone:{type: Number, required:true}
 })
 
-export default mongoose.model('User', User);
+export default mongoose.model('User', User);    
